68d352516985d247053b0ef11b6d224507ec803e
SNill1 <146336277+SNill1@users.noreply.github.com>
2023-09-28 18:16:03 +0300

    Add files via upload

---
+++ MainApp/Program.cs
@@ -0,0 +1,99 @@
+﻿using Bert;
+using System.IO;
+
+
+namespace MyApp
+{
+    class ConsoleUtility
+    {
+        const char _block = '■';
+        const string _back = "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b";
+        const string _twirl = "-\\|/";
+
+        public static void WriteProgressBar(int percent, bool update = false)
+        {
+            if (update)
+                Console.Write(_back);
+            Console.Write("[");
+            var p = (int)((percent / 10f) + .5f);
+            for (var i = 0; i < 10; ++i)
+            {
+                if (i >= p)
+                    Console.Write(' ');
+                else
+                    Console.Write(_block);
+            }
+            Console.Write("] {0,3:##0}%", percent);
+        }
+
+        public static void WriteProgress(int progress, bool update = false)
+        {
+            if (update)
+                Console.Write("\b");
+            Console.Write(_twirl[progress % _twirl.Length]);
+        }
+
+        public static void ProgressBar(long? totalFileSize, long totalBytesDownloaded, double? progressPercentage)
+        {
+            //Console.WriteLine($"Download: {progressPercentage}%, ({totalBytesDownloaded}//{totalFileSize})");
+            int i = (int)progressPercentage;
+            ConsoleUtility.WriteProgressBar(i, true);
+        }
+    }
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Give file path as argument");
+                return;
+            }
+
+            string path = args[0];
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("File does not exist");
+                return ;
+            }
+
+            string text;
+
+            try
+            {
+                text = File.ReadAllText(path);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            CancellationTokenSource cts = new();
+            TaskFactory factory = new();
+
+            Console.WriteLine("Initializing model and checking for neccesary files. Please, wait.\n");
+            await TextAnalyzer.CreateAsync(cts.Token, ConsoleUtility.ProgressBar);
+
+            Console.WriteLine("\nModel is ready.\n");
+
+            Console.WriteLine("Text:\n\n");
+            Console.WriteLine(text + "\n");
+
+            List<Task> TaskList = new();
+            string qst = Console.ReadLine();
+            while (!string.IsNullOrEmpty(qst))
+            {
+                var getAnsTask = factory.StartNew(() => Console.WriteLine(TextAnalyzer.GetAnswerAsync(qst, text).Result),
+                                                cts.Token,
+                                                TaskCreationOptions.LongRunning,
+                                                TaskScheduler.Default);
+                TaskList.Add(getAnsTask);
+                qst = Console.ReadLine();
+            }
+
+            Console.WriteLine("Questions input is finished. Waiting for answers.\n");
+            Task.WaitAll(TaskList.ToArray());
+        }
+    }
+}
